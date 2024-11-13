c4704cbd588ba097e862fd62ed5b5c2c38f8338c
Abhas K <abhas123mnm@gmail.com>
2024-11-13 12:49:53 +0000

    Publish 10 from staging into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -3,20 +3,24 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$$$$$1 from "./apps/deco/records.ts";
+import * as $$$$$$$$$$$2 from "./apps/site.ts";
+import * as $$$$$$3 from "./sections/abhas.tsx";
 import * as $$$$$$0 from "./sections/Footer.tsx";
 import * as $$$$$$1 from "./sections/ItWorks.tsx";
 import * as $$$$$$2 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
+    "site/sections/abhas.tsx": $$$$$$3,
     "site/sections/Footer.tsx": $$$$$$0,
     "site/sections/ItWorks.tsx": $$$$$$1,
     "site/sections/Theme/Theme.tsx": $$$$$$2,
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$1,
+    "site/apps/deco/records.ts": $$$$$$$$$$$1,
+    "site/apps/site.ts": $$$$$$$$$$$2,
   },
   "name": "site",
   "baseUrl": import.meta.url,
